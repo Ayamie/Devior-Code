2ed5ec58c4c49d34b4da7cc194fec1766477e00d
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2020-11-12 13:02:59 +0100

    Update ticket.js

--- code/commands/ticket.js
+++ code/commands/ticket.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const ticketId = '771360580820533258'
+const ticketId = "771360580820533258"
 const check = '✅'
 const roleSupport = "770367479989928006"
 
@@ -8,8 +8,20 @@ module.exports = {
     name: "ticket",
     minArgs: 1,
     expectedArgs: "<message>",
-    execute(message) {
-        const { guild, member, server } = message
+    execute(message, args) {
+        if (message.content === "d.ticket") {
+            const ticketWarn = new Discord.MessageEmbed()
+            .setTitle("Ticket")
+            .setDescription("Merci d'utiliser `d.ticket open` pour ouvrir un ticket ou `d.ticket close le-nom-du-channel-sans-#` pour fermer un ticket.")
+
+            message.channel.send(ticketWarn)
+            message.channel.bulkDelete(1)
+        }
+
+        else if (args[0] === "open") {
+            const { guild, member, server } = message
+    
+        let ticketCreator = message.author.username;
 
         const channel = guild.channels.cache.get(ticketId)
 
@@ -25,10 +37,26 @@ module.exports = {
         
         const ticketMP = new Discord.MessageEmbed()
         .setTitle(`Message`)
-        .setDescription(`Regarde si ton message d'aide à été réagis avec ${check} dans le salon <#${ticketId}>`)
+        .setDescription(`Regarde si ton message d'aide à été réagis avec ${check} dans le salon #${ticketId}`)
 
         message.channel.bulkDelete(1)
+
+        message.guild.channels.create(`${ticketCreator} ticket`)
+        .then(console.log)
+        .catch(console.error);
         
-        message.channel.send(`<@${member.id}>`, ticketMP)
+        message.author.send(`<@${member.id}>`, ticketMP)
+        }
+        else if (args[0] === "close") {
+            if (message.member.hasPermission('MANAGE_CHANNELS')) {
+                const fetchedChannel = message.guild.channels.cache.find(ch => ch.name === `${args[1]}`);
+    
+            fetchedChannel.delete()
+            .catch(err => {
+                console.error(err);
+            });
+            message.channel.bulkDelete(1)
+            }
+        }
     },
 }
